eaa15a31956cceb62e8b3438d3fb04adaf167099
plaul <lmor@kea.dk>
2023-09-21 10:42:23 +0200

    ryder op efter forvirring

--- pages/findCar/findCar.js
+++ pages/findCar/findCar.js
@@ -1,5 +1,23 @@
 
+import {SERVER_URL} from "../../settings.js"
+
 export function initFindCar(){
-  console.log("initFindCar")
+  document.getElementById("result").innerText = ""
+  document.getElementById("btn").addEventListener("click",findCar)
+}
+
+
+async function findCar(){
+  
+  const id = document.getElementById("car-id").value 
+
+  const car = await fetch(SERVER_URL+"/"+id)
+           .then(res=> {
+            if(!res.ok){
+              throw new Error("Car not Found")
+            }
+            return res.json()
+           })
+  document.getElementById("result").innerText = JSON.stringify(car,null,3)
 }
 
